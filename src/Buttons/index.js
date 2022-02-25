2ead07b48f9407860ec72ea689585164c2a796ad
Marcin Malek <marcinmalek01@gmail.com>
2022-02-25 20:15:22 +0100

    Fixed letter case

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,7 +4,7 @@ const Buttons = ({ tasks, hideDoneTasks }) => (
     tasks.length > 0 && (
         <div className="buttons">
             <button className="buttons__button">
-                {hideDoneTasks ? "Pokaż" : "Ukryj"} Ukończone
+                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button className="buttons__button" disabled={tasks.every(({ done }) => done)}>
                 Ukończ wszystkie
