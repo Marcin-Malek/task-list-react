b9dedc833f6fd7449b3fab753d3c9e7640551c4a
Marcin Malek <marcinmalek01@gmail.com>
2022-04-01 06:51:20 +0200

    code cleanup

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,37 +1,21 @@
-import { useSelector } from "react-redux";
 import Form from "./Form";
 import Header from "../../common/Header";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
-import { useTasks } from "../../useTasks";
-import { selectTasks } from "./tasksSlice";
 
-function Tasks() {
-  const { tasks } = useSelector(selectTasks);
-
-  const { addNewTask, setAllDone, toggleTaskDone, removeTask } = useTasks();
-
-  return (
-    <>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        content={<Form addNewTask={addNewTask} />} />
-      <Section
-        title="Lista zadań"
-        content=
-        {<TaskList
-          toggleTaskDone={toggleTaskDone}
-          removeTask={removeTask}
-        />}
-        extraHeaderContent=
-        {<Buttons
-          setAllDone={setAllDone}
-        />}
-      />
-    </>
-  );
-};
+const Tasks = () => (
+  <>
+    <Header title="Lista zadań" />
+    <Section
+      title="Dodaj nowe zadanie"
+      content={<Form />} />
+    <Section
+      title="Lista zadań"
+      content={<TaskList />}
+      extraHeaderContent={<Buttons />}
+    />
+  </>
+);
 
 export default Tasks;
