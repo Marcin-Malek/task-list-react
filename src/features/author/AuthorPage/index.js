29b2bbc428befcdfb4ea71deb3b7a31b0363c980
Marcin Malek <marcinmalek01@gmail.com>
2022-04-05 20:12:14 +0200

    added description about me in AuthorPage

--- src/features/author/AuthorPage/index.js
+++ src/features/author/AuthorPage/index.js
@@ -7,7 +7,20 @@ const AuthorPage = () => (
         <Section
             title="Marcin Małek"
             content={
-                <>Mój opis</>
+                <>
+                    <p>
+                        Nazywam się Marcin Małek, mam 22 lata i jestem absolwentem
+                        <i>Uniwersytetu im. Adama Mickiewicza w Poznaniu</i>.
+                        Ukończylem studia na kierunku <i>Akustyka</i>.
+                        Do końca 2021 roku pracowałem jako specjalista ds. Ochrony przed hałasem.
+                        W pracy zajmowałem się zagadnieniami takimi jak <b>akustyka budowlana</b> i <b>hałas w środowisku</b>.
+                    </p>
+                    <p>
+                        Od Początku 2022 roku jestem mocno zaangażowanym uczestnikiem kursu <i>Frontend developer od podstaw</i>,
+                        który organizowany jest przez zespół YouCode.
+                        Mam nadzieję, że moje determinacja w połączeniu z ciężką pracą pozwoli mi w przyszłości na uzyskanie mojej wymarzonej pracy jako programista. 😀
+                    </p>
+                </>
             }
         />
     </>
